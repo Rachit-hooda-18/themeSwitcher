9bb23e0939b3d6b916053fc29b44198226ededc4
RachitHooda <86191267+Rachit-hooda-18@users.noreply.github.com>
2022-07-30 21:36:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const themeSwitcher = document.querySelector("#themeSwitcher");
+
+navigator.geolocation.getCurrentPosition((position) => {
+  let sunset = new Date().sunset(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+  let sunrise = new Date().sunrise(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+  if (isDay(sunrise, sunset)) {
+    setTheme("theme-light");
+  } else {
+    setTheme("theme-dark");
+  }
+
+  function isDay(sunset, sunrise) {
+    const nowHours = new Date().getHours();
+    return nowHours >= sunrise.getHours() && nowHours < sunset.getHours();
+  }
+});
+
+const defaultTheme = localStorage.getItem("theme") || "theme-light";
+
+setTheme(defaultTheme);
+
+themeSwitcher.addEventListener("change", (e) => {
+  setTheme(e.target.value);
+});
+
+function setTheme(theme) {
+  document.documentElement.className = theme;
+  localStorage.setItem("theme", theme);
+  themeSwitcher.value = theme;
+}
